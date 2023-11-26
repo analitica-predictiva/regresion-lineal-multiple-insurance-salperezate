ba4d6c57b2aa6b4c8c3e4bdffeedd1fde5d18a45
salperezate <salperezate@unal.edu.co>
2023-11-26 13:24:10 -0500

    Correcciones menores

--- preguntas.py
+++ preguntas.py
@@ -81,34 +81,6 @@ def pregunta_03():
     # Importe OneHotEncoder
     from sklearn.preprocessing import OneHotEncoder
 
-    # pipeline = ___(
-    #     steps=[
-    #         # Paso 1: Construya un column_transformer que aplica OneHotEncoder a las
-    #         # variables categóricas, y no aplica ninguna transformación al resto de
-    #         # las variables.
-    #         (
-    #             "column_transfomer",
-    #             ____(
-    #                 (
-    #                     ____(),
-    #                     ____(____=____),
-    #                 ),
-    #                 remainder=____,
-    #             ),
-    #         ),
-    #         # Paso 2: Construya un selector de características que seleccione las K
-    #         # características más importantes. Utilice la función f_regression.
-    #         (
-    #             "selectKBest",
-    #             ____(____=____),
-    #         ),
-    #         # Paso 3: Construya un modelo de regresión lineal.
-    #         (
-    #             "____",
-    #             ____(____),
-    #         ),
-    #     ],
-    # )
     
     pipeline = Pipeline(
         steps=[
@@ -144,9 +116,7 @@ def pregunta_03():
 
     # Defina un diccionario de parámetros para el GridSearchCV. Se deben
     # considerar valores desde 1 hasta 11 regresores para el modelo
-    # param_grid = {
-    #     ____: ____(____, ____),
-    # }
+
     param_grid = {
         "selectKBest__k": range(1, 11),
     }
@@ -154,14 +124,7 @@ def pregunta_03():
     # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
     # parámetros. Use cv = 5, y como métrica de evaluación el valor negativo del
     # error cuadrático medio.
-    # gridSearchCV = ____(
-    #     estimator=____,
-    #     param_grid=____,
-    #     cv=____,
-    #     scoring=____,
-    #     refit=____,
-    #     return_train_score=____,
-    # )
+
     gridSearchCV = GridSearchCV(
         estimator=pipeline,
         param_grid=param_grid,
@@ -195,23 +158,13 @@ def pregunta_04():
     X_train, X_test, y_train, y_test = pregunta_02()
 
     # Evalúe el modelo con los conjuntos de entrenamiento y prueba.
-    #y_train_pred = ____.____(____)
-    #y_test_pred = ____.____(____)
+
     y_train_pred = gridSearchCV.predict(X_train)
     y_test_pred = gridSearchCV.predict(X_test)
 
     # Compute el error cuadratico medio de entrenamiento y prueba. Redondee los
     # valores a dos decimales.
 
-    # mse_train = ____(
-    #     _____,
-    #     _____,
-    # ).round(2)
-
-    # mse_test = ____(
-    #     _____,
-    #     _____,
-    # ).round(2)
     mse_train = mean_squared_error(y_train, y_train_pred).round(2)
     mse_test = mean_squared_error(y_test, y_test_pred).round(2)
 
